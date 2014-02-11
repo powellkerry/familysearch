a60d92ea0f125ce0ae806ac908819209c0d3ddc1
Kerry Powell <kpowell@ldschurch.org>
2014-02-11 09:03:23 -0700

    abstracted and simplified code

--- sort.js
+++ sort.js
@@ -1,51 +1,41 @@
 var sort = {
+    /*
+    Sort an object array based on the config provided
+    @inputArray the array of objects to be sorted
+    @fields an array of configuration objects in the format {field: 'fieldName', direction: 'direction to sort (ASC or DESC)'}
+    returns a new sorted array
+     */
     sortObjectArray: function (inputArray, fields) {
-        var sortedArray = inputArray,
-            first = fields.length >=0 ? fields[0] : null,
-            second = fields.length >= 2 ? fields[1] : null,
-            third = fields.length >= 3 ? fields[2] : null,
-            fourth = fields.length >= 4 ? fields[3] : null;
-        if (first) {
+        var me = this,
+            sortedArray = inputArray;
+        if (fields.length > 0) {
             sortedArray = sortedArray.sort(function (a, b) {
-                if (a[first.field] < b[first.field]) {
-                    return first.direction == 'ASC' ? -1 : 1;
-                } else if (a[first.field] > b[first.field]) {
-                    return first.direction == 'ASC' ? 1 : -1;
-                } else {
-                    if (second) {
-                        if (a[second.field] < b[second.field]) {
-                            return second.direction == 'ASC' ? -1 : 1;
-                        } else if (a[second.field] > b[second.field]) {
-                            return second.direction == 'ASC' ? 1 : -1;
-                        } else {
-                            if (third) {
-                                if (a[third.field] < b[third.field]) {
-                                    return third.direction == 'ASC' ? -1 : 1;
-                                } else if (a[third.field] > b[third.field]) {
-                                    return third.direction == 'ASC' ? 1 : -1;
-                                } else {
-                                    if (fourth) {
-                                        if (a[fourth.field] < b[fourth.field]) {
-                                            return fourth.direction == 'ASC' ? -1 : 1;
-                                        } else if (a[fourth.field] > b[fourth.field]) {
-                                            return fourth.direction == 'ASC' ? 1 : -1;
-                                        } else {
-                                            return 0;
-                                        }
-                                    } else {
-                                        return 0;
-                                    }
-                                }
-                            } else {
-                                return 0;
-                            }
-                        }
-                    } else {
-                        return 0;
-                    }
+                var value = 0,
+                    i = 0;
+                while (value == 0 && i < fields.length) {
+                    value = me.getSortValue(a,b,fields[i]);
+                    i++;
                 }
+                return value;
             });
-            return sortedArray;
+        }
+        return sortedArray;
+    },
+
+    /*
+    Function to return the value used by the sort method
+    @a the first object to compare
+    @b the second object to compare
+    @config the configuration object containing {field: 'fieldName', direction: 'direction to sort (ASC or DESC)'}
+    return 0 if the same, 1 if the object needs to be moved down, and -1 if the object needs to be moved up
+     */
+    getSortValue: function(a,b,config) {
+        if (a[config.field] < b[config.field]) {
+            return config.direction == 'ASC' ? -1 : 1;
+        } else if (a[config.field] > b[config.field]) {
+            return config.direction == 'ASC' ? 1 : -1;
+        } else {
+            return 0
         }
     }
 }
